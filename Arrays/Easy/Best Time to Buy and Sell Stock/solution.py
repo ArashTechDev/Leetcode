97616ee19eb70a680ae082b2cca364f6916ef2ea
Arashdeep Singh Arashdeep Singh <144269515+ArashTechDev@users.noreply.github.com>
2025-05-27 20:07:01 -0400

    Update solution.py

--- Arrays/Easy/Best Time to Buy and Sell Stock/solution.py
+++ Arrays/Easy/Best Time to Buy and Sell Stock/solution.py
@@ -1,1 +1,13 @@
-asdasd
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buyPrice = prices[0]
+        maxProfit = 0
+
+        for i in range(1,len(prices)):
+            if prices[i] < buyPrice:
+                buyPrice = prices[i]
+            else:
+                profit = prices[i] - buyPrice
+                if profit > maxProfit:
+                    maxProfit = profit
+        return maxProfit
